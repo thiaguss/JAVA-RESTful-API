ebb9995a2769831c4c06a776dd1e4e7bbc26a284
Thiaguss <tsantos7098@gmail.com>
2024-06-11 19:32:30 -0300

    first commit

---
+++ src/main/java/edu/APIRESTful/Application.java
@@ -0,0 +1,13 @@
+package edu.APIRESTful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiresTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiresTfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/APIRESTful/ApiresTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.APIRESTful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiresTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
