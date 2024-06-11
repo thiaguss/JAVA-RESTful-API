54e808efcf23a079f0e96d8c341471bcfd6d84d0
Thiaguss <tsantos7098@gmail.com>
2024-06-11 20:34:41 -0300

    Entities

---
+++ src/main/java/edu/APIRESTful/domain/model/Feature.java
@@ -0,0 +1,7 @@
+package edu.APIRESTful.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_feature")
+public class Feature extends BaseItem { }
---
+++ src/main/java/edu/APIRESTful/domain/model/News.java
@@ -0,0 +1,7 @@
+package edu.APIRESTful.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_news")
+public class News extends BaseItem{ }
