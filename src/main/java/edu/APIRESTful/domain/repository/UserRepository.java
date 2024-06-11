0a8e095f42f1f5d7b849d3702e5347c95946c920
Thiaguss <tsantos7098@gmail.com>
2024-06-11 20:40:16 -0300

    Interface UserRepository

---
+++ src/main/java/edu/APIRESTful/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package edu.APIRESTful.domain.repository;
+
+import edu.APIRESTful.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
