41bd30d6026cc365662d88008d449a73595015f6
Thiaguss <tsantos7098@gmail.com>
2024-06-12 04:05:01 -0300

    Created Dtos

---
+++ src/main/java/edu/APIRESTful/web/controller/dto/FeatureDto.java
@@ -0,0 +1,18 @@
+package edu.APIRESTful.web.controller.dto;
+
+import edu.APIRESTful.domain.model.Feature;
+
+public record FeatureDto(Long id, String icon, String description) {
+
+    public FeatureDto(Feature model) {
+        this(model.getId(), model.getIcon(), model.getDescription());
+    }
+
+    public Feature toModel() {
+        Feature model = new Feature();
+        model.setId(this.id);
+        model.setIcon(this.icon);
+        model.setDescription(this.description);
+        return model;
+    }
+}
